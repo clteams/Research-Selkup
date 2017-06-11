9fed256b992d9fa0cda788edc365d4381d9a8ab1
prodotiscus <f.sizov@yandex.ru>
2017-06-11 13:51:52 +0300

    Formatter: production

--- Format_SRDS.py
+++ Format_SRDS.py
@@ -2,8 +2,8 @@
 import pymorphy2
 import re
 
-#srds = open('./srds2.txt').read().splitlines()
-srds = open('tests/f11').read().splitlines()
+srds = open('./srds2.txt').read().splitlines()
+#srds = open('tests/f12').read().splitlines()
 
 def check_type(string):
     RE_slashG = r'\/\w+\.[^\/]*\/'
@@ -33,30 +33,35 @@ def check_type(string):
         return 'slice' # todo: (maybe) add other types, except base/slice
     else:
         return 'base'
-continue_times = 0
-len_srds = len(srds)
-for i in range(len(srds)):
-    cs = srds[i]
-    if continue_times > 0:
-        continue_times -= 1
-        continue
-    if i != len_srds - 1:
-        j = i + 1
-        while check_type(srds[j]) != 'base':
-            cs += " " + srds[j]
-            if j == len_srds - 1:
-                break
-            j += 1
-            continue_times += 1
-    # remove "err- ors"
-    cs = re.sub(r'(?<=[^\s-])-\s', '', cs)
-    # `^ -> 'гх'` 2017-06-10 issue
-    cs = re.sub(r'\^', 'гх', cs)
-    # `rsi 18` issue [not full]
-    cs = re.sub(r'(?<=\w)\d', 'гх', cs)
-    # `rsi 19` issue
-    cs = re.sub(r'(/[^\/]+/)(\d+)', '\g<1> \g<2>', cs)
-    # `rsi 21` issue
-    cs = re.sub(r'(?<=\w)[\d;]+(?=\w)', 'дф', cs)
-    print(cs)
+ix = 0
+with open('srds-formatted.txt', 'a') as o:
+    continue_times = 0
+    len_srds = len(srds)
+    for i in range(len(srds)):
+        cs = srds[i]
+        if continue_times > 0:
+            continue_times -= 1
+            continue
+        if i != len_srds - 1:
+            j = i + 1
+            while check_type(srds[j]) != 'base':
+                cs += " " + srds[j]
+                if j == len_srds - 1:
+                    break
+                j += 1
+                continue_times += 1
+        # remove "err- ors"
+        cs = re.sub(r'(?<=[^\s-])-\s', '', cs)
+        # `^ -> 'гх'` 2017-06-10 issue
+        cs = re.sub(r'\^', 'гх', cs)
+        # `rsi 18` issue [not full]
+        cs = re.sub(r'(?<=\w)\d', 'гх', cs)
+        # `rsi 19` issue
+        cs = re.sub(r'(/[^\/]+/)(\d+)', '\g<1> \g<2>', cs)
+        # `rsi 21` issue
+        cs = re.sub(r'(?<=\w)[\d;]+(?=\w)', 'дф', cs)
+        o.write(cs)
+        print(ix)
+        ix += 1
+
 
