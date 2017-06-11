d789aa493b9b149b77d11b78428a5789286e1a0e
prodotiscus <f.sizov@yandex.ru>
2017-06-11 15:36:24 +0300

    updated `formatter`

--- Format_SRDS.py
+++ Format_SRDS.py
@@ -2,21 +2,25 @@
 import pymorphy2
 import re
 
+class reg:
+    w = r'[\wёқэ́ӓҗӣңёӧи́ӯӱ]'
+    w_lex = w[:-1] + r'\s~]+'
+
 srds = open('./srds2.txt').read().splitlines()
 #srds = open('tests/f12').read().splitlines()
 
 def check_type(string):
-    RE_slashG = r'\/\w+\.[^\/]*\/'
+    RE_slashG = r'\/' + reg.w + r'+\.[^\/]*\/'
     base_patterns = (
-        r'\w+\s+[^\/]*' + RE_slashG,
+        reg.w + r'+\s+[^\/]*' + RE_slashG,
         #/кет./ отгл. гл., пер., С,
-        RE_slashG + r'\s+(\s\w\s|\s\w+\.)',
+        RE_slashG + r'\s+(\s' + reg.w + r'\s|\s' + reg.w + r'+\.)',
     )
     base_no_patterns = (
         #3) проём: мадан ӓк /об. Ш/ «дверной проём»
         RE_slashG + r'\s+[«»]',
-	#2. соответствует отриц. приставке «не»
-	r'^\s*\d+\.'
+        #2. соответствует отриц. приставке «не»
+        r'^\s*\d+\.'
     )
     # check for 'base' type
     base_type = False
@@ -55,11 +59,11 @@ with open('srds-formatted.txt', 'a') as o:
         # `^ -> 'гх'` 2017-06-10 issue
         cs = re.sub(r'\^', 'гх', cs)
         # `rsi 18` issue [not full]
-        cs = re.sub(r'(?<=\w)\d', 'гх', cs)
+        cs = re.sub(r'(?<=' + reg.w + r')\d', 'гх', cs)
         # `rsi 19` issue
         cs = re.sub(r'(/[^\/]+/)(\d+)', '\g<1> \g<2>', cs)
         # `rsi 21` issue
-        cs = re.sub(r'(?<=\w)[\d;]+(?=\w)', 'дф', cs)
+        cs = re.sub(r'(?<=' + reg.w + r')[\d;]+(?=' + reg.w + r')', 'дф', cs)
         o.write(cs + "\n")
         print(ix)
         ix += 1
