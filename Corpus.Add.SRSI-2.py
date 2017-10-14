2ccaccb9e87a89591d56b2d68369ae7155417c80
prodotiscus <example@hamsters.com>
2017-10-14 14:51:26 +0300

    update

--- Corpus.Add.SRSI-2.py
+++ Corpus.Add.SRSI-2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import re
 import CorpusUpdate
-import pymorphy2
+import selkup_alphabet
 
-#my_db = CorpusUpdate.Database()
+my_db = CorpusUpdate.Database()
 srsi_file = open('resources/srsi-2-plain.txt').read().splitlines()
 
 RX_STOP = r'[\.\?!]$'
@@ -48,24 +48,19 @@ def add_to_buffer(num, content, update=False):
 def append_on_buffer():
     Buffer.append({})
 
-## I can't use the debugger and therefore I use prints ;)
 
 for line in srsi_file:
-    print('-')
     if re.search(RX_USEFUL_START, line):
-        print('a')
         Appended = False
         StartNum = int(re.search(r'^\s*(\d+)', line).group(1))
         upd_line = re.sub('^\s*\d+\s*\.\s*', '', line)
         add_to_buffer(StartNum, upd_line)
         Mode = 'started'
     elif (re.search(RX_PAGE_PATTERN, line) or re.search(RX_PAGE_DIV, line)) and not Appended:
-        print('b')
         Mode = 'continue'
         Appended = True
         append_on_buffer()
     elif Mode == 'started':
-        print('c')
         add_to_buffer(StartNum, line, update=True)
     else:
         if not Appended:
@@ -73,4 +68,27 @@ for line in srsi_file:
             Appended = True
         Mode = 'continue'
 
-print(Buffer)
+srsi_strict = selkup_alphabet.Conv.srsi.strict
+
+for BufferSection in Buffer:
+    bsl = len(BufferSection)
+    for Index, Pair in enumerate(BufferSection):
+        prepare = {
+            'text.selkup': selkup_alphabet.Conv.unify(
+                Pair[0],
+                strict=srsi_strict,
+                soft=[],
+                strict_only=True
+            ),
+            'lemmatized.selkup': ['_' for _ in range(len(Pair[0]))],
+            'text.russian': Pair[1],
+            'metadata.source': ['SRSI_2_'],
+            'metadata.date': ['10/14/2017'],
+            'metadata.pushed_by': ['admin']
+        }
+        if Index != bsl - 1:
+            prepare['metadata.sequence'] = ['true']
+        my_db.add_segment(prepare)
+
+my_db.commit()
+my_db.close()
